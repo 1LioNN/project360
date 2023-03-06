ed3b5a8ea3af4a62ba9361127ad30deea783f3d5
Your Name <you@example.com>
2023-03-05 20:37:38 -0500

    Completed endpoints for items_router

--- backend/models/item.js
+++ backend/models/item.js
@@ -6,4 +6,19 @@ export const Item = sequelize.define("Item", {
     type: DataTypes.STRING,
     allowNull: false,
   },
+
+  filepath: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }, 
+  rotate: {
+    type: DataTypes.INTEGER,
+  }, 
+  coordinates: {
+    type: DataTypes.ARRAY(DataTypes.INTEGER),
+  }, 
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
