95fc8035a0a543a27d31c96c442639fe0c16969a
1LioNN <lionsu1995@gmail.com>
2023-03-07 20:25:04 -0500

    Switch from vite to react-scripts

---
+++ project360/src/App.jsx
@@ -0,0 +1,25 @@
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import Home from "./pages/HomePage";
+import EditPage from "./pages/EditPage";
+
+function App() {
+  const { isLoading, error } = useAuth0();
+
+  return (
+    <div className="App h-full">
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <Home />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
+
