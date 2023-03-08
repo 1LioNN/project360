8f29023f30e068546f2109870cee736e6122a1fe
1LioNN <lionsu1995@gmail.com>
2023-03-07 23:02:22 -0500

    FInished User Dashboard page

--- project360/src/App.jsx
+++ project360/src/App.jsx
@@ -1,6 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import Home from "./pages/HomePage";
-import EditPage from "./pages/EditPage";
+import Dashboard from "./pages/Dashboard";
+import Edit from "./pages/EditPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const { isLoading, error } = useAuth0();
@@ -11,7 +13,11 @@ function App() {
       {!error && isLoading && <p>Loading...</p>}
       {!error && !isLoading && (
         <>
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/edit/:roomId" element={<Edit />} />
+          </Routes>
         </>
       )}
     </div>
--- project360/src/index.jsx
+++ project360/src/index.jsx
@@ -3,20 +3,25 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter } from 'react-router-dom';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-console.log(domain);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin}
+      authorizationParams={{
+        redirect_uri : window.location.origin + "/dashboard"
+      }}
     >
       <App />
     </Auth0Provider>
+    </BrowserRouter>
   </React.StrictMode>
   );
 
