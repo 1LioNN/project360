3dda24beb67c31ddbb6c948faa7c2360a31880c6
1LioNN <lionsu1995@gmail.com>
2023-04-02 01:37:34 -0400

    fixed context menu bug

--- project360/src/components/ContextMenu.jsx
+++ project360/src/components/ContextMenu.jsx
@@ -11,40 +11,63 @@ import { useParams } from "react-router-dom";
 
 function ContextMenu({ ContextMenu, models, setModels }) {
   const roomId = useParams().roomId;
+
+  const resetMenu = () => {
+    ContextMenu.current.ref = null;
+    ContextMenu.current.id = "";
+    ContextMenu.current.style.display = "none";
+    ContextMenu.current.setBBox(null);
+    ContextMenu.current.setCenter(null);
+  };
+
   const deleteItem = () => {
     console.log("delete");
     console.log(ContextMenu.current.id);
-    
+
     apiService.deleteItem(roomId, ContextMenu.current.id).then((res) => {
-      const newModels = models.filter((model) => model.id !== parseInt(ContextMenu.current.id));
+      const newModels = models.filter(
+        (model) => model.id !== parseInt(ContextMenu.current.id)
+      );
       setModels(newModels);
-      ContextMenu.current.ref = null;
-      ContextMenu.current.id = "";
-      ContextMenu.current.style.display = "none";
-      ContextMenu.current.setBBox(null);
-      ContextMenu.current.setCenter(null);
-    })
+      resetMenu();
+    }).catch((e) => {
+      resetMenu();
+    });
   };
 
   const rotateC = () => {
     console.log("rotateC");
     console.log(ContextMenu.current.id);
-    
-    const model = ContextMenu.current.ref.current;
-    model.rotation.y -= Math.PI / 4;
-    if (model.rotation.y <= -2 * Math.PI) {
-      model.rotation.y = 0;
+    //if rotation is null, close context menu
+    try {
+      const model = ContextMenu.current.ref.current;
+      if (model.rotation.y === null) {
+        ContextMenu.current.style.display = "none";
+        return;
+      }
+      model.rotation.y -= Math.PI / 4;
+      if (model.rotation.y <= -2 * Math.PI) {
+        model.rotation.y = 0;
+      }
+      const bbox = new THREE.Box3().setFromObject(model);
+      ContextMenu.current.setBBox(bbox);
+      ContextMenu.current.setCenter(
+        bbox.max
+          .clone()
+          .sub(bbox.min)
+          .multiplyScalar(1 / 2)
+      );
+      apiService.updateItemAng(ContextMenu.current.id, model.rotation.y);
+    } catch (e) {
+      resetMenu();
     }
-    const bbox = new THREE.Box3().setFromObject(model);
-    ContextMenu.current.setBBox(bbox);
-    ContextMenu.current.setCenter(bbox.max.clone().sub(bbox.min).multiplyScalar(1/2));
-    apiService.updateItemAng(ContextMenu.current.id, model.rotation.y);
   };
 
   const rotateCC = () => {
     console.log("rotateCC");
     console.log(ContextMenu.current.id);
 
+    try {
     const model = ContextMenu.current.ref.current;
     model.rotation.y += Math.PI / 4;
     if (model.rotation.y >= 2 * Math.PI) {
@@ -52,8 +75,16 @@ function ContextMenu({ ContextMenu, models, setModels }) {
     }
     const bbox = new THREE.Box3().setFromObject(model);
     ContextMenu.current.setBBox(bbox);
-    ContextMenu.current.setCenter(bbox.max.clone().sub(bbox.min).multiplyScalar(1/2));
+    ContextMenu.current.setCenter(
+      bbox.max
+        .clone()
+        .sub(bbox.min)
+        .multiplyScalar(1 / 2)
+    );
     apiService.updateItemAng(ContextMenu.current.id, model.rotation.y);
+    } catch (e) {
+      resetMenu();
+    }
   };
 
   return (
