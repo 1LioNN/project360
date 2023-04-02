a3f6d7fcb54ed21e3c195279581c091b9f4032de
Lander Joshua Vitug <lander.vitug@gmail.com>
2023-04-02 16:34:28 -0400

    rename Auth0Provider to Auth0ProviderWithNav

--- project360/src/index.jsx
+++ project360/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import { Auth0Provider } from "./Auth0Provider.jsx";
+import App from "./App.jsx";
+import Auth0ProviderWithNav from "./Auth0ProviderWithNav.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
 import "primereact/resources/primereact.css"; // core css
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider>
+      <Auth0ProviderWithNav>
         <App />
-      </Auth0Provider>
+      </Auth0ProviderWithNav>
     </BrowserRouter>
   </React.StrictMode>
 );
