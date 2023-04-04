8366d4e78c9433fd4f55857049a404653b1efa52
1LioNN <lionsu1995@gmail.com>
2023-04-04 16:16:06 -0400

    fix urls

--- project360/src/socketConnect.js
+++ project360/src/socketConnect.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "project360.me:5001";
+const URL = "api.project360.me:5001";
 
 export const socket = io.connect(URL, {
   withCredentials: true,
