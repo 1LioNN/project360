c2f3d713abf8538e3590f3f0a38d4a61785109bd
1LioNN <lionsu1995@gmail.com>
2023-04-04 15:12:58 -0400

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const clients = {};
 
 app.use(
   cors({
-    origin: `http://localhost`,
+    origin: `https://project360.me`,
     credentials: true,
   })
 );
@@ -34,7 +34,7 @@ app.use(
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost",
+    origin: `https://project360.me`,
     credentials: true,
   },
 });
